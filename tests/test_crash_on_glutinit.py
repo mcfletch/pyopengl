5cd9a0d27244b5f253f07c3e390eacc827a418bd
U-sturm\mcfletch <mcfletch@sturm>
2009-02-16 21:33:23 -0500

    Fix missing raise on attribute error for function lookup, fix usage of glut in test case, fix permissions so that win32 dlls can be run from a bzr checkout (hopefully)

--- tests/test_crash_on_glutinit.py
+++ tests/test_crash_on_glutinit.py
@@ -1,6 +1,9 @@
 
 if __name__ == "__main__":
 	from OpenGL.GLUT import *
+	from OpenGL.GL import *
 	glutInit( ' ' )
 	glutInitDisplayMode( GLUT_SINGLE )
+	window = glutCreateWindow("hello")
+	glutDisplayFunc( lambda *args: 1 )
 	glutMainLoop()
