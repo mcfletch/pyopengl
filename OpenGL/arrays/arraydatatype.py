6d40577e734450a4ee3ae57761dec80be9b60a81
Mike C. Fletcher <mcfletch@sturm>
2009-04-29 15:02:36 -0400

    Changes to support accelerated array data type and format handler along with some refactoring

--- OpenGL/arrays/arraydatatype.py
+++ OpenGL/arrays/arraydatatype.py
@@ -6,162 +6,248 @@ from OpenGL.arrays import formathandler
 from OpenGL import logs
 log = logs.getLog( 'OpenGL.arrays.arraydatatype' )
 
-class ArrayDatatype( object ):
-	"""Mix-in for array datatype classes
+
+
+try:
+	from OpenGL_accelerate.arraydatatype import ArrayDatatype as ADT
+except ImportError, err:
+	# Python-coded version
+	log.info( 'Using unaccelerated ArrayDatatype' )
+	class HandlerRegistry( dict ):
+		GENERIC_OUTPUT_PREFERENCES = ['numpy','numeric','ctypesarrays']
+		def __init__( self, plugin_match ):
+			self.match = plugin_match
+			self.output_handler = None 
+			self.preferredOutput = None
+			self.all_output_handlers = []
+		def __call__( self, value ):
+			"""Lookup of handler for given value"""
+			try:
+				typ = value.__class__
+			except AttributeError, err:
+				typ = type(value)
+			handler = self.get( typ )
+			if not handler:
+				if hasattr( typ, '__mro__' ):
+					for base in typ.__mro__:
+						handler = self.get( base )
+						if not handler:
+							handler = self.match( base )
+						if handler:
+							handler = self[ base ]
+							self[ typ ] = handler 
+							if hasattr( handler, 'registerEquivalent' ):
+								handler.registerEquivalent( typ, base )
+							return handler
+				raise TypeError(
+					"""No array-type handler for type %r (value: %s) registered"""%(
+						typ, repr(value)[:50]
+					)
+				)
+			return handler
+		
+		def get_output_handler( self ):
+			"""Fast-path lookup for output handler object"""
+			if self.output_handler is None:
+				if not self:
+					formathandler.FormatHandler.loadAll()
+				if self.preferredOutput is not None:
+					self.output_handler = self.get( self.preferredOutput )
+				if not self.output_handler:
+					for preferred in self.GENERIC_OUTPUT_PREFERENCES:
+						self.output_handler = self.get( preferred )
+						if self.output_handler:
+							break
+				if not self.output_handler:
+					# look for anything that can do output...
+					for handler in self.all_output_handlers:
+						self.output_handler = handler 
+						break
+				if not self.output_handler:
+					raise RuntimeError(
+						"""Unable to find any output handler at all (not even ctypes/numpy ones!)"""
+					)
+			return self.output_handler
+		
+		def register( self, handler, types=None ):
+			"""Register this class as handler for given set of types"""
+			if not isinstance( types, (list,tuple)):
+				types = [ types ]
+			for type in types:
+				self[ type ] = handler
+			if handler.isOutput:
+				self.all_output_handlers.append( handler )
+			
+		def registerReturn( self, handler ):
+			"""Register this handler as the default return-type handler"""
+			self.preferredOutput = handler 
+			self.output_handler = None
 	
-	The ArrayDatatype marker essentially is used to mark a particular argument
-	as having an "array" type, which means that it is eligible for handling 
-	via the arrays sub-package and its registered handlers.
-	"""
-	typeConstant = None
-	def getHandler( cls, value ):
-		"""Retrieve FormatHandler for given value 
-		
-		This method is replaced by the FormatHandler registry
-		once the registry is initialized...
-		"""
-		formathandler.FormatHandler.loadAll()
-		ArrayDatatype.getHandler = formathandler.FormatHandler.TYPE_REGISTRY
-		return cls.getHandler( value )
-	getHandler = classmethod( getHandler )
-	def from_param( cls, value ):
-		"""Given a value in a known data-pointer type, convert to a ctypes pointer"""
-		return cls.getHandler(value).from_param( value, cls.typeConstant )
-	from_param = classmethod( logs.logOnFail( from_param, log ) )
-	def dataPointer( cls, value ):
-		"""Given a value in a known data-pointer type, return long for pointer"""
-		try:
-			return cls.getHandler(value).dataPointer( value )
-		except Exception, err:
-			log.warn(
-				"""Failure in dataPointer for %s instance %s""", type(value), value,
-			)
-			raise
-	dataPointer = classmethod( logs.logOnFail( dataPointer, log ) )
-	def voidDataPointer( cls, value ):
-		"""Given value in a known data-pointer type, return void_p for pointer"""
-		return ctypes.c_void_p( cls.dataPointer( value ))
-	voidDataPointer = classmethod( logs.logOnFail( voidDataPointer, log ) )
-	def typedPointer( cls, value ):
-		"""Return a pointer-to-base-type pointer for given value"""
-		return ctypes.cast( cls.dataPointer(value), ctypes.POINTER( cls.baseType ))
-	typedPointer = classmethod( typedPointer )
-	def asArray( cls, value, typeCode=None ):
-		"""Given a value, convert to preferred array representation"""
-		return cls.getHandler(value).asArray( value, typeCode or cls.typeConstant )
-	asArray = classmethod( logs.logOnFail( asArray, log ) )
-	def arrayToGLType( cls, value ):
-		"""Given a data-value, guess the OpenGL type of the corresponding pointer
-		
-		Note: this is not currently used in PyOpenGL and may be removed 
-		eventually.
-		"""
-		return cls.getHandler(value).arrayToGLType( value )
-	arrayToGLType = classmethod( logs.logOnFail( arrayToGLType, log ) )
-	def arraySize( cls, value, typeCode = None ):
-		"""Given a data-value, calculate dimensions for the array (number-of-units)"""
-		return cls.getHandler(value).arraySize( value, typeCode or cls.typeConstant )
-	arraySize = classmethod( logs.logOnFail( arraySize, log ) )
-	def unitSize( cls, value, typeCode=None ):
-		"""Determine unit size of an array (if possible)
-		
-		Uses our local type if defined, otherwise asks the handler to guess...
-		"""
-		return cls.getHandler(value).unitSize( value, typeCode or cls.typeConstant )
-	unitSize = classmethod( logs.logOnFail( unitSize, log ) )
-	def returnHandler( cls ):
-		"""Get the default return-handler"""
-		try:
-			return formathandler.FormatHandler.RETURN_HANDLER
-		except AttributeError, err:
-			if not formathandler.FormatHandler.TYPE_REGISTRY:
-				formathandler.FormatHandler.loadAll()
-			return formathandler.FormatHandler.chooseOutput( )
-	returnHandler = classmethod( logs.logOnFail( returnHandler, log ) )
-	def zeros( cls, dims, typeCode=None ):
-		"""Allocate a return array of the given dimensions filled with zeros"""
-		return cls.returnHandler().zeros( dims, typeCode or cls.typeConstant )
-	zeros = classmethod( logs.logOnFail( zeros, log ) )
-	def dimensions( cls, value ):
-		"""Given a data-value, get the dimensions (assumes full structure info)"""
-		return cls.getHandler(value).dimensions( value )
-	dimensions = classmethod( logs.logOnFail( dimensions, log ) )
+	GLOBAL_REGISTRY = HandlerRegistry( plugins.FormatHandler.match)
+	formathandler.FormatHandler.TYPE_REGISTRY = GLOBAL_REGISTRY
 	
-	def arrayByteCount( cls, value ):
-		"""Given a data-value, try to determine number of bytes it's final form occupies
+	class ArrayDatatype( object ):
+		"""Mix-in for array datatype classes
 		
-		For most data-types this is arraySize() * atomic-unit-size
+		The ArrayDatatype marker essentially is used to mark a particular argument
+		as having an "array" type, which means that it is eligible for handling 
+		via the arrays sub-package and its registered handlers.
 		"""
-		return cls.getHandler(value).arrayByteCount( value )
-	arrayByteCount = classmethod( logs.logOnFail( arrayByteCount, log ) )
+		typeConstant = None
+		handler = GLOBAL_REGISTRY
+		getHandler = GLOBAL_REGISTRY.__call__
+		returnHandler = GLOBAL_REGISTRY.get_output_handler
+		@classmethod
+		def getRegistry( cls ):
+			"""Get our handler registry"""
+			return cls.handler 
+		def from_param( cls, value ):
+			"""Given a value in a known data-pointer type, convert to a ctypes pointer"""
+			return cls.getHandler(value).from_param( value, cls.typeConstant )
+		from_param = classmethod( logs.logOnFail( from_param, log ) )
+		def dataPointer( cls, value ):
+			"""Given a value in a known data-pointer type, return long for pointer"""
+			try:
+				return cls.getHandler(value).dataPointer( value )
+			except Exception, err:
+				log.warn(
+					"""Failure in dataPointer for %s instance %s""", type(value), value,
+				)
+				raise
+		dataPointer = classmethod( logs.logOnFail( dataPointer, log ) )
+		def voidDataPointer( cls, value ):
+			"""Given value in a known data-pointer type, return void_p for pointer"""
+			return ctypes.c_void_p( cls.dataPointer( value ))
+		voidDataPointer = classmethod( logs.logOnFail( voidDataPointer, log ) )
+		def typedPointer( cls, value ):
+			"""Return a pointer-to-base-type pointer for given value"""
+			return ctypes.cast( cls.dataPointer(value), ctypes.POINTER( cls.baseType ))
+		typedPointer = classmethod( typedPointer )
+		def asArray( cls, value, typeCode=None ):
+			"""Given a value, convert to preferred array representation"""
+			return cls.getHandler(value).asArray( value, typeCode or cls.typeConstant )
+		asArray = classmethod( logs.logOnFail( asArray, log ) )
+		def arrayToGLType( cls, value ):
+			"""Given a data-value, guess the OpenGL type of the corresponding pointer
+			
+			Note: this is not currently used in PyOpenGL and may be removed 
+			eventually.
+			"""
+			return cls.getHandler(value).arrayToGLType( value )
+		arrayToGLType = classmethod( logs.logOnFail( arrayToGLType, log ) )
+		def arraySize( cls, value, typeCode = None ):
+			"""Given a data-value, calculate dimensions for the array (number-of-units)"""
+			return cls.getHandler(value).arraySize( value, typeCode or cls.typeConstant )
+		arraySize = classmethod( logs.logOnFail( arraySize, log ) )
+		def unitSize( cls, value, typeCode=None ):
+			"""Determine unit size of an array (if possible)
+			
+			Uses our local type if defined, otherwise asks the handler to guess...
+			"""
+			return cls.getHandler(value).unitSize( value, typeCode or cls.typeConstant )
+		unitSize = classmethod( logs.logOnFail( unitSize, log ) )
+		def zeros( cls, dims, typeCode=None ):
+			"""Allocate a return array of the given dimensions filled with zeros"""
+			return cls.returnHandler().zeros( dims, typeCode or cls.typeConstant )
+		zeros = classmethod( logs.logOnFail( zeros, log ) )
+		def dimensions( cls, value ):
+			"""Given a data-value, get the dimensions (assumes full structure info)"""
+			return cls.getHandler(value).dimensions( value )
+		dimensions = classmethod( logs.logOnFail( dimensions, log ) )
 		
+		def arrayByteCount( cls, value ):
+			"""Given a data-value, try to determine number of bytes it's final form occupies
+			
+			For most data-types this is arraySize() * atomic-unit-size
+			"""
+			return cls.getHandler(value).arrayByteCount( value )
+		arrayByteCount = classmethod( logs.logOnFail( arrayByteCount, log ) )
+			
 
-# the final array data-type classes...
-class GLclampdArray( ArrayDatatype, ctypes.POINTER(constants.GLclampd )):
-	"""Array datatype for GLclampd types"""
-	baseType = constants.GLclampd
-	typeConstant = constants.GL_DOUBLE
+	# the final array data-type classes...
+	class GLclampdArray( ArrayDatatype, ctypes.POINTER(constants.GLclampd )):
+		"""Array datatype for GLclampd types"""
+		baseType = constants.GLclampd
+		typeConstant = constants.GL_DOUBLE
 
-class GLclampfArray( ArrayDatatype, ctypes.POINTER(constants.GLclampf )):
-	"""Array datatype for GLclampf types"""
-	baseType = constants.GLclampf
-	typeConstant = constants.GL_FLOAT
+	class GLclampfArray( ArrayDatatype, ctypes.POINTER(constants.GLclampf )):
+		"""Array datatype for GLclampf types"""
+		baseType = constants.GLclampf
+		typeConstant = constants.GL_FLOAT
 
-class GLfloatArray( ArrayDatatype, ctypes.POINTER(constants.GLfloat )):
-	"""Array datatype for GLfloat types"""
-	baseType = constants.GLfloat
-	typeConstant = constants.GL_FLOAT
+	class GLfloatArray( ArrayDatatype, ctypes.POINTER(constants.GLfloat )):
+		"""Array datatype for GLfloat types"""
+		baseType = constants.GLfloat
+		typeConstant = constants.GL_FLOAT
 
-class GLdoubleArray( ArrayDatatype, ctypes.POINTER(constants.GLdouble )):
-	"""Array datatype for GLdouble types"""
-	baseType = constants.GLdouble
-	typeConstant = constants.GL_DOUBLE
+	class GLdoubleArray( ArrayDatatype, ctypes.POINTER(constants.GLdouble )):
+		"""Array datatype for GLdouble types"""
+		baseType = constants.GLdouble
+		typeConstant = constants.GL_DOUBLE
 
-class GLbyteArray( ArrayDatatype, ctypes.POINTER(constants.GLbyte )):
-	"""Array datatype for GLbyte types"""
-	baseType = constants.GLbyte
-	typeConstant = constants.GL_BYTE
+	class GLbyteArray( ArrayDatatype, ctypes.POINTER(constants.GLbyte )):
+		"""Array datatype for GLbyte types"""
+		baseType = constants.GLbyte
+		typeConstant = constants.GL_BYTE
 
-class GLcharArray( ArrayDatatype, ctypes.c_char_p):
-	"""Array datatype for ARB extension pointers-to-arrays"""
-	baseType = constants.GLchar
-	typeConstant = constants.GL_BYTE
-GLcharARBArray = GLcharArray
+	class GLcharArray( ArrayDatatype, ctypes.c_char_p):
+		"""Array datatype for ARB extension pointers-to-arrays"""
+		baseType = constants.GLchar
+		typeConstant = constants.GL_BYTE
+	GLcharARBArray = GLcharArray
 
-class GLshortArray( ArrayDatatype, ctypes.POINTER(constants.GLshort )):
-	"""Array datatype for GLshort types"""
-	baseType = constants.GLshort
-	typeConstant = constants.GL_SHORT
+	class GLshortArray( ArrayDatatype, ctypes.POINTER(constants.GLshort )):
+		"""Array datatype for GLshort types"""
+		baseType = constants.GLshort
+		typeConstant = constants.GL_SHORT
 
-class GLintArray( ArrayDatatype, ctypes.POINTER(constants.GLint )):
-	"""Array datatype for GLint types"""
-	baseType = constants.GLint
-	typeConstant = constants.GL_INT
+	class GLintArray( ArrayDatatype, ctypes.POINTER(constants.GLint )):
+		"""Array datatype for GLint types"""
+		baseType = constants.GLint
+		typeConstant = constants.GL_INT
 
-class GLubyteArray( ArrayDatatype, ctypes.POINTER(constants.GLubyte )):
-	"""Array datatype for GLubyte types"""
-	baseType = constants.GLubyte
-	typeConstant = constants.GL_UNSIGNED_BYTE
-GLbooleanArray = GLubyteArray
+	class GLubyteArray( ArrayDatatype, ctypes.POINTER(constants.GLubyte )):
+		"""Array datatype for GLubyte types"""
+		baseType = constants.GLubyte
+		typeConstant = constants.GL_UNSIGNED_BYTE
+	GLbooleanArray = GLubyteArray
 
-class GLushortArray( ArrayDatatype, ctypes.POINTER(constants.GLushort )):
-	"""Array datatype for GLushort types"""
-	baseType = constants.GLushort
-	typeConstant = constants.GL_UNSIGNED_SHORT
+	class GLushortArray( ArrayDatatype, ctypes.POINTER(constants.GLushort )):
+		"""Array datatype for GLushort types"""
+		baseType = constants.GLushort
+		typeConstant = constants.GL_UNSIGNED_SHORT
 
-class GLuintArray( ArrayDatatype, ctypes.POINTER(constants.GLuint )):
-	"""Array datatype for GLuint types"""
-	baseType = constants.GLuint
-	typeConstant = constants.GL_UNSIGNED_INT
+	class GLuintArray( ArrayDatatype, ctypes.POINTER(constants.GLuint )):
+		"""Array datatype for GLuint types"""
+		baseType = constants.GLuint
+		typeConstant = constants.GL_UNSIGNED_INT
 
-class GLenumArray( ArrayDatatype, ctypes.POINTER(constants.GLenum )):
-	"""Array datatype for GLenum types"""
-	baseType = constants.GLenum
-	typeConstant = constants.GL_UNSIGNED_INT
-class GLsizeiArray( ArrayDatatype, ctypes.POINTER(constants.GLsizei )):
-	"""Array datatype for GLenum types"""
-	baseType = constants.GLsizei
-	typeConstant = constants.GL_INT
+	class GLenumArray( ArrayDatatype, ctypes.POINTER(constants.GLenum )):
+		"""Array datatype for GLenum types"""
+		baseType = constants.GLenum
+		typeConstant = constants.GL_UNSIGNED_INT
+	class GLsizeiArray( ArrayDatatype, ctypes.POINTER(constants.GLsizei )):
+		"""Array datatype for GLenum types"""
+		baseType = constants.GLsizei
+		typeConstant = constants.GL_INT
+else:
+	# Cython-coded array handler
+	log.info( 'Using accelerated ArrayDatatype' )
+	ArrayDatatype = ADT( None, None )
+	GLclampdArray = ADT( constants.GL_DOUBLE, constants.GLclampd )
+	GLclampfArray = ADT( constants.GL_FLOAT, constants.GLclampf )
+	GLdoubleArray = ADT( constants.GL_DOUBLE, constants.GLdouble )
+	GLfloatArray = ADT( constants.GL_FLOAT, constants.GLfloat )
+	GLbyteArray = ADT( constants.GL_BYTE, constants.GLbyte )
+	GLcharArray = GLcharARBArray = ADT( constants.GL_BYTE, constants.GLchar )
+	GLshortArray = ADT( constants.GL_SHORT, constants.GLshort )
+	GLintArray = ADT( constants.GL_INT, constants.GLint )
+	GLubyteArray = GLbooleanArray = ADT( constants.GL_UNSIGNED_BYTE, constants.GLubyte )
+	GLushortArray = ADT( constants.GL_UNSIGNED_SHORT, constants.GLushort )
+	GLuintArray = ADT( constants.GL_UNSIGNED_INT, constants.GLuint )
+	GLenumArray = ADT( constants.GL_UNSIGNED_INT, constants.GLenum )
+	GLsizeiArray = ADT( constants.GL_INT, constants.GLsizei )
+
 
 GL_CONSTANT_TO_ARRAY_TYPE = {
 	constants.GL_DOUBLE : GLclampdArray,
--- OpenGL/arrays/formathandler.py
+++ OpenGL/arrays/formathandler.py
@@ -5,43 +5,6 @@ Data-type handlers are specified using OpenGL.plugins module
 import ctypes
 from OpenGL import plugins
 
-try:
-#	raise ImportError( 'Testing without wrapper' )
-	from OpenGL_accelerate.wrapper import HandlerRegistry
-except ImportError, err:
-	class HandlerRegistry( dict ):
-		c_api = False
-		def __init__( self, match ):
-			self.match = match 
-		def __call__( self, value ):
-			try:
-				typ = value.__class__
-			except AttributeError, err:
-				# Bug report that at least one type of array can 
-				# be passed in without __class__ available, likely 
-				# on an older Python or with old Numeric.
-				typ = type(value)
-			handler = self.get( typ )
-			if handler is None:
-				if hasattr( typ, '__mro__' ):
-					for base in typ.__mro__:
-						handler = self.get( base )
-						if handler is None:
-							handler = self.match( base )
-							if handler:
-								handler = handler.load()
-						if handler:
-							self[ base ] = handler
-							handler.registerEquivalent( typ, base )
-							self[ typ ] = handler 
-							return handler
-				raise TypeError(
-					"""No array-type handler for type %r (value: %s) registered"""%(
-						typ, repr(value)[:50]
-					)
-				)
-			return handler
-
 class FormatHandler( object ):
 	"""Abstract class describing the handler interface
 	
@@ -49,7 +12,7 @@ class FormatHandler( object ):
 	which allow it to manipulate (and create) instances of the data-type 
 	it represents.
 	"""
-	TYPE_REGISTRY = HandlerRegistry(plugins.FormatHandler.match)
+	TYPE_REGISTRY = None # HandlerRegistry(plugins.FormatHandler.match)
 	LAZY_TYPE_REGISTRY = {}  # more registrations
 	HANDLER_REGISTRY = {}
 	baseType = None
@@ -71,6 +34,7 @@ class FormatHandler( object ):
 			],
 		}
 		"""
+		assert FormatHandler.TYPE_REGISTRY is not None
 		for entrypoint in plugins.FormatHandler.all():
 			cls.loadPlugin( entrypoint )
 	@classmethod
@@ -93,15 +57,13 @@ class FormatHandler( object ):
 			else:
 				handler = plugin_class()
 				handler.register( handler.HANDLED_TYPES )
-				cls.HANDLER_REGISTRY[ entrypoint.name ] = handler
-				if handler.isOutput:
-					cls.ALL_OUTPUT_HANDLERS.append( handler )
+				cls.TYPE_REGISTRY[ entrypoint.name ] = handler 
 			entrypoint.loaded = True
 	@classmethod
 	def typeLookup( cls, type ):
 		"""Lookup handler by data-type"""
 		try:
-			return cls.TYPE_REGISTRY[ type ]
+			return FormatHandler.TYPE_REGISTRY[ type ]
 		except KeyError, err:
 			key = '%s.%s'%(type.__module__,type.__name__)
 			plugin = cls.LAZY_TYPE_REGISTRY.get( key )
@@ -110,47 +72,15 @@ class FormatHandler( object ):
 				return cls.TYPE_REGISTRY[ type ]
 			raise KeyError( """Unable to find data-format handler for %s"""%( type,))
 	loadAll = classmethod( loadAll )
-	@classmethod
-	def chooseOutput( cls, preferred=None ):
-		"""Initialize and setup our output type"""
-		cls.loadAll()
-		cls.chooseOutput = cls.chooseOutput_final
-		return cls.chooseOutput( preferred )
-	@classmethod
-	def chooseOutput_final( cls, preferred=None ):
-		"""Choose our output-capable plugin"""
-		if preferred is not None:
-			cls.preferredOutput = preferred
-		handler = None
-		handler = cls.HANDLER_REGISTRY.get( cls.preferredOutput )
-		if not handler:
-			for preferred in cls.GENERIC_OUTPUT_PREFERENCES:
-				handler = cls.HANDLER_REGISTRY.get( preferred )
-				if handler:
-					break
-		if not handler:
-			# look for anything that can do output...
-			for handler in cls.ALL_OUTPUT_HANDLERS:
-				break
-		if not handler:
-			raise RuntimeError(
-				"""Unable to find any output handler at all (not even ctypes/numpy ones!)"""
-			)
-		handler.registerReturn()
-		return handler
-	
+
+
 	def register( self, types=None ):
 		"""Register this class as handler for given set of types"""
-		if not isinstance( types, (list,tuple)):
-			types = [ types ]
-		for type in types:
-			FormatHandler.TYPE_REGISTRY[ type ] = self
+		FormatHandler.TYPE_REGISTRY.register( self, types )
 	def registerReturn( self ):
 		"""Register this handler as the default return-type handler"""
-		FormatHandler.RETURN_HANDLER = self
-	def registerEquivalent( self, typ, base ):
-		"""Register a sub-class for handling as the base-type"""
-		
+		FormatHandler.TYPE_REGISTRY.registerReturn( self )
+
 	def from_param( self, value, typeCode=None  ):
 		"""Convert to a ctypes pointer value"""
 	def dataPointer( self, value ):
