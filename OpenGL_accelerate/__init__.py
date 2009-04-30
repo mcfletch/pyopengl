45a131f7a47d135334da1eedf66e6126bfd6fef2
Mike C. Fletcher <mcfletch@sturm>
2009-04-30 19:56:48 -0400

    Add a bit of a doc-string

---
+++ OpenGL_accelerate/__init__.py
@@ -0,0 +1,8 @@
+"""Cython-coded accelerators for the PyOpenGL wrapper
+
+This package contains Cython accelerator modules which 
+attempt to speed up certain aspects of the PyOpenGL 3.x
+wrapper mechanism.  The source code is part of the 
+PyOpenGL package and is built via the setupaccel.py
+script in the top level of the PyOpenGL source package.
+"""
