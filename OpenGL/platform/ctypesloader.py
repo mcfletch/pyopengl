d0ff364fe8a48fd004a6f07706492cd250180b35
Mike C. Fletcher <mcfletch@sturm>
2009-07-08 13:52:37 -0400

    Remove support for ctypes 0.9.9.3, make ctypes loader log with module dotted name

--- OpenGL/platform/ctypesloader.py
+++ OpenGL/platform/ctypesloader.py
@@ -4,10 +4,10 @@ We keep rewriting functions as the main entry points change,
 so let's just localise the changes here...
 """
 import ctypes, logging, os
-log = logging.getLogger( 'OpenGL.ctypes' )
-log.setLevel( logging.DEBUG )
+log = logging.getLogger( 'OpenGL.platform.ctypesloader' )
+#log.setLevel( logging.DEBUG )
 ctypes_version = [
-	int(x) for x in ctypes.__version__.split('.')
+    int(x) for x in ctypes.__version__.split('.')
 ]
 from ctypes import util
 import OpenGL
@@ -17,57 +17,50 @@ DLL_DIRECTORY = os.path.join( os.path.dirname( OpenGL.__file__ ), 'DLLS' )
 
 
 def loadLibrary( dllType, name, mode = ctypes.RTLD_GLOBAL ):
-	"""Load a given library by name with the given mode
-	
-	dllType -- the standard ctypes pointer to a dll type, such as
-		ctypes.cdll or ctypes.windll or the underlying ctypes.CDLL or 
-		ctypes.WinDLL classes.
-	name -- a short module name, e.g. 'GL' or 'GLU'
-	mode -- ctypes.RTLD_GLOBAL or ctypes.RTLD_LOCAL,
-		controls whether the module resolves names via other
-		modules already loaded into this process.  GL modules
-		generally need to be loaded with GLOBAL flags
-	
-	returns the ctypes C-module object
-	"""
-	if ctypes_version > [0,9,9,3]:
-		if isinstance( dllType, ctypes.LibraryLoader ):
-			dllType = dllType._dlltype
-		fullName = None
-		try:
-			fullName = util.find_library( name )
-			if fullName is not None:
-				name = fullName
-			elif os.path.isfile( os.path.join( DLL_DIRECTORY, name + '.dll' )):
-				name = os.path.join( DLL_DIRECTORY, name + '.dll' )
-		except Exception, err:
-			log.info( '''Failed on util.find_library( %r ): %s''', name, err )
-			# Should the call fail, we just try to load the base filename...
-			pass
-		try:
-			return dllType( name, mode )
-		except Exception, err:
-			err.args += (name,fullName)
-			raise
-	elif ctypes_version == [0,9,9,3]:
-		return dllType.find( name, mode )
-	else:
-		raise RuntimeError(
-			"""Unsupported ctypes version (%s), please upgrade to at least ctypes 0.9.9.3"""%(
-				".".join( [str(x) for x in ctypes_version] ),
-			)
-		)
+    """Load a given library by name with the given mode
+    
+    dllType -- the standard ctypes pointer to a dll type, such as
+        ctypes.cdll or ctypes.windll or the underlying ctypes.CDLL or 
+        ctypes.WinDLL classes.
+    name -- a short module name, e.g. 'GL' or 'GLU'
+    mode -- ctypes.RTLD_GLOBAL or ctypes.RTLD_LOCAL,
+        controls whether the module resolves names via other
+        modules already loaded into this process.  GL modules
+        generally need to be loaded with GLOBAL flags
+    
+    returns the ctypes C-module object
+    """
+    if ctypes_version <= [0,9,9,3]:
+        raise RuntimeError(
+            """Unsupported ctypes version (%s), please upgrade to ctypes 1.x or above"""%(
+                ".".join( [str(x) for x in ctypes_version] ),
+            )
+        )
+    if isinstance( dllType, ctypes.LibraryLoader ):
+        dllType = dllType._dlltype
+    fullName = None
+    try:
+        fullName = util.find_library( name )
+        if fullName is not None:
+            name = fullName
+        elif os.path.isfile( os.path.join( DLL_DIRECTORY, name + '.dll' )):
+            name = os.path.join( DLL_DIRECTORY, name + '.dll' )
+    except Exception, err:
+        log.info( '''Failed on util.find_library( %r ): %s''', name, err )
+        # Should the call fail, we just try to load the base filename...
+        pass
+    try:
+        return dllType( name, mode )
+    except Exception, err:
+        err.args += (name,fullName)
+        raise
 
 def buildFunction( functionType, name, dll ):
-	"""Abstract away the ctypes function-creation operation"""
-	if ctypes_version > [0,9,9,3]:
-		return functionType( (name, dll), )
-	elif ctypes_version == [0,9,9,3]:
-		return functionType( name, dll )
-	else:
-		raise RuntimeError(
-			"""Unsupported ctypes version (%s), please upgrade to at least ctypes 0.9.9.3"""%(
-				".".join( [str(x) for x in ctypes_version] ),
-			)
-		)
-
+    """Abstract away the ctypes function-creation operation"""
+    if ctypes_version <= [0,9,9,3]:
+        raise RuntimeError(
+            """Unsupported ctypes version (%s), please upgrade to 1.x or above"""%(
+                ".".join( [str(x) for x in ctypes_version] ),
+            )
+        )
+    return functionType( (name, dll), )
