0cb6b4b4f79cdfa53092e6184eda0a4ce2122fd2
Mike C. Fletcher <mcfletch@vrplumber.com>
2010-04-03 23:15:29 -0400

    Fix SF#2980896, deallocation function was using XDECREF on the assumption that setting None would set NULL for the object type, it actually sets the Py_NONE instance, which caused decref failures.

--- OpenGL_accelerate/src/vbo.pyx
+++ OpenGL_accelerate/src/vbo.pyx
@@ -11,23 +11,23 @@ _NULL = object()
 
 cdef class VBO:
     """Instances can be passed into array-handling routines
-    
+
     You can check for whether VBOs are supported by accessing the implementation
-    attribute of the VBO, which will raise a RuntimeError if there is no available 
+    attribute of the VBO, which will raise a RuntimeError if there is no available
     implementation.
-    
+
     Attributes:
-    
-        int copied -- whether we are copied to back-end yet 
-        int created -- whether we've created a buffer yet 
-        unsigned int buffer -- our buffer once created 
-        object data -- our data-holding array-compatible object 
-        target -- our resolved GL constant target 
-        target_spec -- our (unresolved) GL constant specifier 
-        usage -- our resolved GL constant usage 
-        usage_spec -- our (unresolved) GL constant usage specifier 
-        _copy_segments -- slices of data to copy to back-end 
-        _I_ -- our implementation object 
+
+        int copied -- whether we are copied to back-end yet
+        int created -- whether we've created a buffer yet
+        unsigned int buffer -- our buffer once created
+        object data -- our data-holding array-compatible object
+        target -- our resolved GL constant target
+        target_spec -- our (unresolved) GL constant specifier
+        usage -- our resolved GL constant usage
+        usage_spec -- our (unresolved) GL constant usage specifier
+        _copy_segments -- slices of data to copy to back-end
+        _I_ -- our implementation object
         arrayType -- our reference to arraydatatype.ArrayDatatype
     """
     cdef object __weakref__ # allow weak-referencing for cleanups...
@@ -38,56 +38,58 @@ cdef class VBO:
     cdef public int size
     cdef public object target
     cdef public object usage
-    cdef public int resolved 
+    cdef public int resolved
     cdef public object target_spec # possible string definition
     cdef public object usage_spec # possible string definition
     cdef public list _copy_segments
     cdef public object _I_
     cdef public object arrayType
     _no_cache_ = True # do not cache in context data arrays
-    def __init__( 
-        self, data, usage='GL_DYNAMIC_DRAW', 
+    def __init__(
+        self, data, usage='GL_DYNAMIC_DRAW',
         target='GL_ARRAY_BUFFER',
         size=None,
     ):
-        """Initialize the VBO 
-        
-        data -- array-compatible data format object 
-        
-        usage -- string/GLenum constant specifying streaming usage 
+        """Initialize the VBO
+
+        data -- array-compatible data format object
+
+        usage -- string/GLenum constant specifying streaming usage
             for the VBO, normal values:
-            
+
                 GL_STREAM_DRAW -- updated every frame from client to card
-                GL_STREAM_COPY -- read and written from card each frame 
-                
+                GL_STREAM_COPY -- read and written from card each frame
+
                 GL_STATIC_DRAW -- just written from client (once)
-                GL_STATIC_READ -- just read from client 
+                GL_STATIC_READ -- just read from client
                 GL_STATIC_COPY -- read and written from client (once)
-                
-                GL_DYNAMIC_DRAW -- updated from client every once in a while 
+
+                GL_DYNAMIC_DRAW -- updated from client every once in a while
                 GL_DYNAMIC_COPY -- read and written from client every once in a while
-                GL_DYNAMIC_READ -- read from the client every once in a while 
-        
-        target -- string/GLenum constant specifying the role for the buffer 
+                GL_DYNAMIC_READ -- read from the client every once in a while
+
+        target -- string/GLenum constant specifying the role for the buffer
             normal values:
-            
-                GL_ARRAY_BUFFER -- vertex attribute values 
-                GL_ELEMENT_ARRAY_BUFFER -- vertex index values 
-                GL_PIXEL_PACK_BUFFER/GL_PIXEL_UNPACK_BUFFER -- client-side 
+
+                GL_ARRAY_BUFFER -- vertex attribute values
+                GL_ELEMENT_ARRAY_BUFFER -- vertex index values
+                GL_PIXEL_PACK_BUFFER/GL_PIXEL_UNPACK_BUFFER -- client-side
                     image source/sink for image-manipulation operations.
         """
         from OpenGL.arrays.arraydatatype import ArrayDatatype
         self.arrayType = ArrayDatatype
         self.c_set_array( data, size )
-        self.resolved = self.created = self.copied = False 
-        self.usage_spec = usage 
-        self.target_spec = target 
+        self.resolved = self.created = self.copied = False
+        self.usage_spec = usage
+        self.target_spec = target
         self._copy_segments = []
         self._I_ = None
     def __dealloc__( self ):
         """Deallocate our references"""
-        Py_XDECREF( self.data )
-    @property 
+        if self.data is not None:
+            Py_XDECREF( self.data )
+            self.data = None
+    @property
     def implementation( self ):
         """Retrieve our implementation reference"""
         return self.get_implementation()
@@ -100,9 +102,9 @@ cdef class VBO:
     cdef unsigned int c_resolve( self, value ):
         """Resolve string constant to constant"""
         if isinstance( value, (str,unicode)):
-            return getattr( 
-                self.get_implementation(), 
-                self.get_implementation().basename( value ) 
+            return getattr(
+                self.get_implementation(),
+                self.get_implementation().basename( value )
             )
         return value
     def set_array( self, data, size=None ):
@@ -110,33 +112,34 @@ cdef class VBO:
         return self.c_set_array( data, size )
     cdef c_set_array( self, data, size ):
         """Set our array pointer with incref/decref support"""
-        Py_XDECREF( self.data )
+        if self.data is not None:
+            Py_XDECREF( self.data )
         Py_XINCREF( data )
-        self.data = data 
+        self.data = data
         if size is None and self.data is not None:
             size = self.arrayType.arrayByteCount( self.data )
-        self.size = size 
+        self.size = size
         self.copied = False
     def __setitem__( self, slice, array):
         """Set slice of data on the array and vbo (if copied already)
-        
-        slice -- the Python slice object determining how the data should 
-            be copied into the vbo/array 
-        array -- something array-compatible that will be used as the 
-            source of the data, note that the data-format will have to 
-            be the same as the internal data-array to work properly, if 
+
+        slice -- the Python slice object determining how the data should
+            be copied into the vbo/array
+        array -- something array-compatible that will be used as the
+            source of the data, note that the data-format will have to
+            be the same as the internal data-array to work properly, if
             not, the amount of data copied will be wrong.
-        
+
         This is a reasonably complex operation, it has to have all sorts
         of state-aware changes to correctly map the source into the low-level
-        OpenGL view of the buffer (which is just bytes as far as the GL 
+        OpenGL view of the buffer (which is just bytes as far as the GL
         is concerned).
         """
         if slice.step and not slice.step == 1:
             raise NotImplemented( """Don't know how to map stepped arrays yet""" )
         # TODO: handle e.g. mapping character data into an integer data-set
         data = self.arrayType.asArray( array )
-        start = (slice.start or 0) 
+        start = (slice.start or 0)
         stop = (slice.stop or len(self.data))
         if start < 0:
             start += len(self.data)
@@ -152,12 +155,12 @@ cdef class VBO:
                 # re-copy the whole data-set
                 self.copied = False
             elif len(data):
-                # now the fun part, we need to make the array match the 
-                # structure of the array we're going to copy into and make 
+                # now the fun part, we need to make the array match the
+                # structure of the array we're going to copy into and make
                 # the "size" parameter match the value we're going to copy in,
-                # note that a 2D array (rather than a 1D array) may require 
+                # note that a 2D array (rather than a 1D array) may require
                 # multiple mappings to copy into the memory area...
-                
+
                 # find the step size from the dimensions and base size...
                 size = self.arrayType.arrayByteCount( data ) / len(array)
                 #baseSize = self.arrayType.unitSize( data )
@@ -186,9 +189,9 @@ cdef class VBO:
         self.target = self.c_resolve( self.target_spec )
         self.usage = self.c_resolve( self.usage_spec )
         self.created = True
-        self.get_implementation()._DELETERS_[ id(self) ] = weakref.ref( 
+        self.get_implementation()._DELETERS_[ id(self) ] = weakref.ref(
             # Cython instances can't have weakrefs, sigh...
-            self, 
+            self,
             self.get_implementation().deleter( [self.buffer], id(self) )
         )
         return self.buffer
@@ -203,7 +206,7 @@ cdef class VBO:
                     self.get_implementation().glBufferSubData(self.target, start, size, dataptr)
         else:
             self.get_implementation().glBufferData(
-                self.target, 
+                self.target,
                 self.size,
                 self.data,
                 self.usage,
@@ -233,13 +236,13 @@ cdef class VBO:
     def __add__( self, other ):
         """Add an integer to this VBO (offset)"""
         if hasattr( other, 'offset' ):
-            other = other.offset 
+            other = other.offset
         assert isinstance( other, (int,long) ), """Only know how to add integer/long offsets"""
         return VBOOffset( self, other )
     cdef int check_live( self ):
         if self.data is _NULL:
             raise RuntimeError( """Attempting to use a deleted VBO""" )
-    
+
     __enter__ = bind
     def __exit__( self, exc_type=None, exc_val=None, exc_tb=None ):
         """Context manager exit"""
@@ -249,29 +252,30 @@ cdef class VBO:
 
 cdef class VBOOffset:
     """Offset into a VBO instance
-    
-    This is just a convenience object that lets you say 
-    vbo + integer and have the value passed as the correct 
-    data-type instead of being interpreted as a pointer to 
+
+    This is just a convenience object that lets you say
+    vbo + integer and have the value passed as the correct
+    data-type instead of being interpreted as a pointer to
     an integer-array of the offset.
     """
-    cdef public VBO vbo 
+    cdef public VBO vbo
     cdef public unsigned int offset
     def __cinit__( self, VBO vbo, unsigned int offset ):
-        self.vbo = vbo 
+        self.vbo = vbo
         Py_XINCREF( vbo )
-        self.offset = offset 
+        self.offset = offset
     def __dealloc__( self ):
-        Py_XDECREF( self.vbo )
-        self.vbo = None
-        
+        if self.vbo is not None:
+            Py_XDECREF( self.vbo )
+            self.vbo = None
+
     def __getattr__( self, key ):
         if key != 'vbo':
             return getattr( self.vbo, key )
         raise AttributeError( 'No %r key in VBOOffset'%(key,))
     def __add__( self, other ):
         if hasattr( other, 'offset' ):
-            other = other.offset 
+            other = other.offset
         return VBOOffset( self.vbo, self.offset + other )
     cdef int check_live( self ):
         if self.vbo is not None:
@@ -281,7 +285,7 @@ cdef class VBOOffset:
 
 cdef class VBOHandler(FormatHandler):
     """Handles VBO instances passed in as array data"""
-    cdef object vp0 
+    cdef object vp0
     cdef object arrayType # import and use explicit reference...
     isOutput = False
     def __init__( self ):
@@ -327,7 +331,7 @@ cdef class VBOOffsetHandler(FormatHandler):
     def __init__( self ):
         from OpenGL.arrays.arraydatatype import ArrayDatatype
         self.arrayType = ArrayDatatype
-    
+
     cdef object c_dataPointer( self, object instance ):
         """Retrieve data-pointer directly"""
         (<VBOOffset>instance).check_live()
@@ -340,7 +344,7 @@ cdef class VBOOffsetHandler(FormatHandler):
         """Retrieve the given value as a (contiguous) array of type typeCode"""
         (<VBOOffset>instance).check_live()
         return instance
-        
+
     cdef c_arrayByteCount( self, object instance ):
         """Given a data-value, calculate number of bytes required to represent"""
         (<VBOOffset>instance).check_live()
