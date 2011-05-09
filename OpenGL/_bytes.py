e0d530a041ce46ada6d75a9aa5fc35c985bc4854
Mike C. Fletcher <mcfletch@vrplumber.com>
2011-05-09 14:00:06 -0400

    Sigh, more attempts to get around the broken 2.6/2.7 to 3.x str changes.

--- OpenGL/_bytes.py
+++ OpenGL/_bytes.py
@@ -1,7 +1,11 @@
 """8-bit string definition for Python 2/3 compatibility"""
 try:
     bytes = bytes
-    _NULL_8_BYTE = bytes( '\000','latin1' )
+    try:
+        _NULL_8_BYTE = bytes( '\000','latin1' )
+    except TypeError, err:
+        # 2.6 or 2.7, where bytes is the old str object
+        _NULL_8_BYTE = bytes( '\000' )
     def as_8_bit( x, encoding='utf-8' ):
         if isinstance( x,unicode ):
             return x.encode(encoding)
