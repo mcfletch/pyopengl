c71f3e193922f77787b24b8a243dfc79e141d516
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-06-30 10:46:35 -0400

    Rework to just use what we know to be true about the Python versions, rather than attempting to construct tests for everything...

--- OpenGL/_bytes.py
+++ OpenGL/_bytes.py
@@ -1,19 +1,48 @@
-"""8-bit string definition for Python 2/3 compatibility"""
-try:
-    bytes = bytes
-    try:
-        _NULL_8_BYTE = bytes( '\000','latin1' )
-    except TypeError, err:
-        # 2.6 or 2.7, where bytes is the old str object
-        _NULL_8_BYTE = bytes( '\000' )
-    def as_8_bit( x, encoding='utf-8' ):
-        if isinstance( x,unicode ):
-            return x.encode(encoding)
-        return str(x).encode( encoding )
-except NameError, err:
+"""8-bit string definitions for Python 2/3 compatibility
+
+Defines the following which allow for dealing with Python 3 breakages:
+
+    STR_IS_BYTES
+    STR_IS_UNICODE
+    
+        Easily checked booleans for type identities
+    
+    _NULL_8_BYTE
+    
+        An 8-bit byte with NULL (0) value 
+    
+    as_8_bit( x, encoding='utf-8')
+    
+        Returns the value as the 8-bit version
+    
+    unicode -- always pointing to the unicode type 
+    bytes -- always pointing to the 8-bit bytes type
+"""
+import sys
+
+STR_IS_BYTES = True
+
+if sys.version_info[:2] < (2,6):
+    # no bytes, traditional setup...
     bytes = str 
+else:
+    bytes = bytes
+if sys.version_info[:2] < (3,0):
+    # traditional setup, with bytes defined...
+    unicode = unicode
     _NULL_8_BYTE = '\000'
     def as_8_bit( x, encoding='utf-8' ):
         if isinstance( x, unicode ):
             return x.encode( encoding )
         return bytes( x ) 
+else:
+    # new setup, str is now unicode...
+    STR_IS_BYTES = False
+    _NULL_8_BYTE = bytes( '\000','latin1' )
+    def as_8_bit( x, encoding='utf-8' ):
+        if isinstance( x,unicode ):
+            return x.encode(encoding)
+        return str(x).encode( encoding )
+    unicode = str
+
+STR_IS_UNICODE = not STR_IS_BYTES
