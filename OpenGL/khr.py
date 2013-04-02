f40efa9a718b95547d6cb58e7356997e81a37bde
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-04-01 23:00:26 -0400

    Create individual opaque structure types with pointer-types for them that have some convenience methods...

--- OpenGL/khr.py
+++ OpenGL/khr.py
@@ -6,22 +6,42 @@ EGLBoolean = ctypes.c_uint32
 EGLenum = ctypes.c_uint32
 EGLint = c_int = ctypes.c_int32
 
-EGLConfig = ctypes.c_size_t
-EGLContext = ctypes.c_size_t
-EGLDisplay = ctypes.c_size_t
-EGLSurface = ctypes.c_size_t
-EGLClientBuffer = ctypes.c_size_t
-EGLImageKHR = ctypes.c_size_t
+class _Opaque( ctypes.Structure ):
+    """An Opaque Structure reference (base class)"""
+class _opaque_pointer( ctypes.POINTER( _Opaque ) ):
+    _type_ = _Opaque
+    @classmethod
+    def from_param( cls, value ):
+        return ctypes.cast( value, cls )
+    @property
+    def address( self ):
+        return ctypes.addressof( self.contents )
+    @property 
+    def as_voidp( self ):
+        return ctypes.c_voidp( self.address )
+
+def _opaque_pointer_cls( name ):
+    """Create an Opaque pointer class for the given name"""
+    typ = type( name, (_Opaque,), {} )
+    p_typ = type( name+'_pointer', (_opaque_pointer,), {'_type_':typ})
+    return p_typ
+
+EGLConfig = _opaque_pointer_cls( 'EGLConfig' )
+EGLContext = _opaque_pointer_cls( 'EGLContext' )
+EGLDisplay = _opaque_pointer_cls( 'EGLDisplay' )
+EGLSurface = _opaque_pointer_cls( 'EGLSurface' )
+EGLClientBuffer = _opaque_pointer_cls( 'EGLClientBuffer' )
+EGLImageKHR = _opaque_pointer_cls( 'EGLImageKHR' )
 
 EGLScreenMESA = ctypes.c_ulong
 EGLModeMESA = ctypes.c_ulong
 
 EGLNativeFileDescriptorKHR = ctypes.c_int
 
-EGLSyncKHR = EGLSyncNV = ctypes.c_voidp
+EGLSyncKHR = EGLSyncNV = _opaque_pointer_cls( 'EGLSync' )
 EGLTimeKHR = EGLTimeNV = ctypes.c_ulonglong
 EGLuint64KHR = EGLuint64NV = ctypes.c_ulonglong
-EGLStreamKHR = ctypes.c_voidp
+EGLStreamKHR = _opaque_pointer_cls( 'EGLStream' )
 EGLsizeiANDROID = ctypes.c_size_t
 
 class EGLClientPixmapHI( ctypes.Structure):
@@ -36,13 +56,11 @@ class wl_display( ctypes.Structure):
     _fields_ = []
 
 # These are X11... no good, really...
-EGLNativeDisplayType = ctypes.c_size_t # Display *
-EGLNativePixmapType = ctypes.c_size_t # Pixmap 
-EGLNativeWindowType = ctypes.c_size_t # Window
+EGLNativeDisplayType = _opaque_pointer_cls( 'EGLNativeDisplayType' )
+EGLNativePixmapType = _opaque_pointer_cls( 'EGLNativePixmapType' )
+EGLNativeWindowType = _opaque_pointer_cls( 'EGLNativeWindowType' )
 
 NativeDisplayType = EGLNativeDisplayType 
 NativePixmapType = EGLNativePixmapType
 NativeWindowType = EGLNativeWindowType
 
-del ctypes 
-del pointer 
