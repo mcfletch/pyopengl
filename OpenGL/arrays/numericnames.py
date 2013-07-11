923e589fe8f4223bb5031835b7eaf1a29ec4b5f0
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-07-11 00:54:49 -0400

    Remove unused numericnames module

--- OpenGL/arrays/numericnames.py
+++
@@ -1,9 +0,0 @@
-"""Module to provide numeric-module name for current version"""
-def moduleName( module ):
-    """Given a numpy, numeric or numarray module, calculate helper module name"""
-    name = module.__name__.lower()
-    version = '_'.join( module.__version__.split('.'))
-    return 'OpenGL.arrays._%(name)s_%(version)s'%locals()
-def loadModule( name ):
-    """Load the current C module for our numeric version"""
-    return __import__( name, {}, {}, name.split( '.' ))
