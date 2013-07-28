d29af2f3a27252595f89abeb834eca3c475e3889
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-07-28 15:15:22 -0400

    Basically working numpy buffer-based api

--- OpenGL/arrays/numpymodule.py
+++ OpenGL/arrays/numpymodule.py
@@ -18,6 +18,59 @@ class NumpyHandler( buffers.BufferHandler ):
     def zeros( cls, dims, typeCode ):
         """Return Numpy array of zeros in given size"""
         return numpy.zeros( dims, GL_TYPE_TO_ARRAY_MAPPING[typeCode])
+    
+    @classmethod
+    def asArray( cls, value, typeCode=None ):
+        """Convert given value to an array value of given typeCode"""
+        return super(NumpyHandler,cls).asArray( cls.contiguous(value,typeCode), typeCode )
+    @classmethod
+    def contiguous( cls, source, typeCode=None ):
+        """Get contiguous array from source
+
+        source -- numpy Python array (or compatible object)
+            for use as the data source.  If this is not a contiguous
+            array of the given typeCode, a copy will be made,
+            otherwise will just be returned unchanged.
+        typeCode -- optional 1-character typeCode specifier for
+            the numpy.array function.
+
+        All gl*Pointer calls should use contiguous arrays, as non-
+        contiguous arrays will be re-copied on every rendering pass.
+        Although this doesn't raise an error, it does tend to slow
+        down rendering.
+        """
+        typeCode = GL_TYPE_TO_ARRAY_MAPPING[ typeCode ]
+        try:
+            contiguous = source.flags.contiguous
+        except AttributeError as err:
+            if typeCode:
+                return numpy.ascontiguousarray( source, typeCode )
+            else:
+                return numpy.ascontiguousarray( source )
+        else:
+            if contiguous and (typeCode is None or typeCode==source.dtype.char):
+                return source
+            elif (contiguous and cls.ERROR_ON_COPY):
+                from OpenGL import error
+                raise error.CopyError(
+                    """Array of type %r passed, required array of type %r""",
+                    source.dtype.char, typeCode,
+                )
+            else:
+                # We have to do astype to avoid errors about unsafe conversions
+                # XXX Confirm that this will *always* create a new contiguous array
+                # XXX Guard against wacky conversion types like uint to float, where
+                # we really don't want to have the C-level conversion occur.
+                # XXX ascontiguousarray is apparently now available in numpy!
+                if cls.ERROR_ON_COPY:
+                    from OpenGL import error
+                    raise error.CopyError(
+                        """Non-contiguous array passed""",
+                        source,
+                    )
+                if typeCode is None:
+                    typeCode = source.dtype.char
+                return numpy.ascontiguousarray( source, typeCode )
 #    
 #from OpenGL._bytes import long, integer_types
 #
