e537bf656260c06238390fe049c9d05d4c56908b
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-11-08 15:53:41 -0500

    Change location of the errors import

--- OpenGL/GL/__init__.py
+++ OpenGL/GL/__init__.py
@@ -6,7 +6,6 @@ from OpenGL.GL.pointers import *
 from OpenGL.GL.images import *
 
 from OpenGL.GL.exceptional import *
-from OpenGL.error import *
 
 from OpenGL.GL.glget import *
 
@@ -27,6 +26,7 @@ from OpenGL.GL.VERSION.GL_4_3 import *
 
 from OpenGL.constants import *
 
+from OpenGL.error import *
 GLerror = GLError
 
 # Now the aliases...
