60a4a914a47041dd055790e0fe4d671cfeebb9f5
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-01-24 15:11:32 -0500

    Wrong, do *not* set extension NULL, is_core is sufficient

--- OpenGL/platform/baseplatform.py
+++ OpenGL/platform/baseplatform.py
@@ -138,8 +138,6 @@ class BasePlatform( object ):
         raises AttributeError if can't find the procedure...
         """
         is_core = (not extension) or extension.split('_')[1] == 'VERSION'
-        if is_core:
-            extension = None
         if (not is_core) and not self.checkExtension( extension ):
             raise AttributeError( """Extension not available""" )
         argTypes = [ self.finalArgType( t ) for t in argTypes ]
