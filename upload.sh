fa6c1c3e6ee8f527d7a73c6ce7af832d1242b264
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-01-30 16:43:01 -0500

    Sync the new files instead of the old ones

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-rsync -avP -e ssh manual-3.0/* mcfletch,pyopengl@web.sourceforge.net:htdocs/documentation/manual-3.0/
+rsync -avP -e ssh manual-3.1/* mcfletch,pyopengl@web.sourceforge.net:htdocs/documentation/manual-3.0/
