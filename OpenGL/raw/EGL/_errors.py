7e9d6b4be5a7c659e8233fc8f45708718e50d27c
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-02-23 02:11:35 -0500

    Add an actual error checker to EGL functions

--- OpenGL/raw/EGL/_errors.py
+++ OpenGL/raw/EGL/_errors.py
@@ -1,3 +1,8 @@
 from OpenGL.error import _ErrorChecker
 from OpenGL.platform import PLATFORM as _p
-_error_checker = _ErrorChecker( _p, None )
+
+_error_checker = _ErrorChecker( 
+    _p, 
+    _p.EGL.eglGetError, 
+    0x3000 # EGL_SUCCESS
+)
