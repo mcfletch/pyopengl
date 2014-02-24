892e5e79ae31d926e20d7663740dcededa67bec6
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-02-24 10:53:27 -0500

    Wrong module, old function introduced during refactoring

--- OpenGL/raw/GLX/_types.py
+++ OpenGL/raw/GLX/_types.py
@@ -1,4 +1,4 @@
-from OpenGL import platform, constant, extensions
+from OpenGL import platform as _p, constant, extensions
 from ctypes import *
 from OpenGL.raw.GL._types import *
 from OpenGL._bytes import as_8_bit
@@ -258,6 +258,4 @@ class GLXHyperpipeConfigSGIX( Structure ):
         ('participationType',c_uint),
         ('timeSlice',c_int),
     ]
-
-_error_function = getattr(_p.GDI32,'GetLastError',None)
 
