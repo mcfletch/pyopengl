9d2e7566f77232502f2406d1f9e6a74894cf7afa
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-04-16 12:46:33 -0400

    Missing import of the error module

--- OpenGL/arrays/strings.py
+++ OpenGL/arrays/strings.py
@@ -4,7 +4,7 @@ from OpenGL.raw.GL import _types
 from OpenGL.raw.GL.VERSION import GL_1_1
 from OpenGL.arrays import formathandler
 import ctypes
-from OpenGL import _bytes
+from OpenGL import _bytes, error
 from OpenGL._configflags import ERROR_ON_COPY
 
 def dataPointer( value, typeCode=None ):
