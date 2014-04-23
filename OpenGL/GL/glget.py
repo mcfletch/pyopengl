07f2dfb4f7152263e0f3c4b97ec32c539dace048
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-04-23 12:02:11 -0400

    Automated wrapping of the map-sizes allows us to eliminate manual wrapper here...

--- OpenGL/GL/glget.py
+++ OpenGL/GL/glget.py
@@ -17,35 +17,8 @@ GLsize = GLsizei = ctypes.c_int
 
 __all__ = (
     'glGetString',
-    'glGetPixelMapfv','glGetPixelMapusv','glGetPixelMapuiv',
 )
 
 glGetString = _simple.glGetString
 glGetString.restype = ctypes.c_char_p
 glGetString.__doc__ = """glGetString( constant ) -> Current string value"""
-
-PIXEL_MAP_SIZE_CONSTANT_MAP = {
-    _simple.GL_PIXEL_MAP_A_TO_A: _simple.GL_PIXEL_MAP_A_TO_A_SIZE,
-    _simple.GL_PIXEL_MAP_B_TO_B: _simple.GL_PIXEL_MAP_B_TO_B_SIZE,
-    _simple.GL_PIXEL_MAP_G_TO_G: _simple.GL_PIXEL_MAP_G_TO_G_SIZE,
-    _simple.GL_PIXEL_MAP_I_TO_A: _simple.GL_PIXEL_MAP_I_TO_A_SIZE,
-    _simple.GL_PIXEL_MAP_I_TO_B: _simple.GL_PIXEL_MAP_I_TO_B_SIZE,
-    _simple.GL_PIXEL_MAP_I_TO_G: _simple.GL_PIXEL_MAP_I_TO_G_SIZE,
-    _simple.GL_PIXEL_MAP_I_TO_I: _simple.GL_PIXEL_MAP_I_TO_I_SIZE,
-    _simple.GL_PIXEL_MAP_I_TO_R: _simple.GL_PIXEL_MAP_I_TO_R_SIZE,
-    _simple.GL_PIXEL_MAP_R_TO_R: _simple.GL_PIXEL_MAP_R_TO_R_SIZE,
-    _simple.GL_PIXEL_MAP_S_TO_S: _simple.GL_PIXEL_MAP_S_TO_S_SIZE,
-}
-def GL_GET_PIXEL_MAP_SIZE( pname ):
-    """Given a pname, lookup the size using a glGet query..."""
-    constant = PIXEL_MAP_SIZE_CONSTANT_MAP[ pname ]
-    return _simple.glGetIntegerv( constant )
-glGetPixelMapfv = wrapper.wrapper(_simple.glGetPixelMapfv).setOutput(
-    "values",GL_GET_PIXEL_MAP_SIZE, "map", orPassIn=True
-)
-glGetPixelMapuiv = wrapper.wrapper(_simple.glGetPixelMapuiv).setOutput(
-    "values",GL_GET_PIXEL_MAP_SIZE, "map", orPassIn=True
-)
-glGetPixelMapusv = wrapper.wrapper(_simple.glGetPixelMapusv).setOutput(
-    "values",GL_GET_PIXEL_MAP_SIZE, "map", orPassIn=True
-)
