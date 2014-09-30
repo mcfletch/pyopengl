eda972dcada6b0840c3f9b0bf428543ab952b881
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-09-30 15:06:31 -0400

    Refactoring error for reference to generic arrayDataType

--- OpenGL/GL/images.py
+++ OpenGL/GL/images.py
@@ -14,6 +14,7 @@
 """
 from OpenGL.raw.GL.VERSION import GL_1_1,GL_1_2, GL_3_0
 from OpenGL import images, arrays, wrapper
+from OpenGL.arrays import arraydatatype
 from OpenGL._bytes import bytes,integer_types
 from OpenGL.raw.GL import _types
 import ctypes
@@ -497,7 +498,7 @@ class CompressedImageConverter( object ):
     def __call__( self, pyArgs, index, wrappedOperation ):
         """Create a data-size measurement for our image"""
         arg = pyArgs[ self.dataIndex ]
-        return arrays.ArrayType.arrayByteCount( arg )
+        return arraydatatype.ArrayDatatype.arrayByteCount(arg)
 
 
 
