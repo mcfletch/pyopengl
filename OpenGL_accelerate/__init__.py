8bab71741e496a951aa39a2a7da3e747ca835468
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-02-13 15:03:14 -0500

    Bump the accelerate version too

--- OpenGL_accelerate/__init__.py
+++ OpenGL_accelerate/__init__.py
@@ -6,5 +6,5 @@ wrapper mechanism.  The source code is part of the
 PyOpenGL package and is built via the setupaccel.py
 script in the top level of the PyOpenGL source package.
 """
-__version__ = '3.1.0'
-__version_tuple__ = (3,1,0)
+__version__ = '3.1.1a1'
+__version_tuple__ = (3,1,1)
