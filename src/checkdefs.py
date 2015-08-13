a02c333de6b287e6012941cb8b68c348b41dd552
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-08-13 09:49:13 -0400

    Allow for testing osmesa and egl when checking definitions

--- src/checkdefs.py
+++ src/checkdefs.py
@@ -2,13 +2,26 @@
 import logging, os, subprocess
 log = logging.getLogger( 'checker' )
 def main():
+    env = running_env = os.environ.copy()
     for path,dirs,files in os.walk( os.path.join('..','OpenGL','raw') ):
         for file in files:
             if file.endswith( '.py' ):
                 log.debug( 'Check: %s', file )
+                if 'osmesa' in path.split(os.sep):
+                    running_env = env.copy()
+                    running_env['PYOPENGL_PLATFORM'] = 'osmesa'
+                elif 'EGL' in path.split(os.sep):
+                    running_env = env.copy()
+                    running_env['PYOPENGL_PLATFORM'] = 'egl'
+                elif 'WGL' in path.split(os.sep):
+                    log.debug("Skipping win32")
+                    continue
+                    
+                else:
+                    running_env = env
                 try:
-                    subprocess.check_call( ['python', os.path.join( path, file )] )
-                except subprocess.CalledProcessError as err:
+                    subprocess.check_call( ['python', os.path.join( path, file )],env=running_env )
+                except subprocess.CalledProcessError:
                     log.error( 'Failure loading: %s/%s', path, file )
 
 if __name__ == "__main__":
