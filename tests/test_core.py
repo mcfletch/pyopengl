01476933527b9e397073b9eca8b34dc67c733c47
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-08-13 09:57:07 -0400

    Note the core-dump on ubuntu

--- tests/test_core.py
+++ tests/test_core.py
@@ -985,6 +985,7 @@ class Tests( unittest.TestCase ):
     if array:
         def test_draw_bitmap_pixels( self ):
             """SF#2152623 Drawing pixels as bitmaps (bits)"""
+            # this core-dumps on Mesa Intel on Ubuntu 15.04 :(
             pixels = array([0,0,0,0,0,0,0,0],'B')
             glDrawPixels( 8,8, GL_COLOR_INDEX, GL_BITMAP, pixels )
     
