76d82a434dcef21d120211cf41bb1f57ae192a9e
Mike C. Fletcher <mcfletch@vrplumber.com>
2017-08-06 19:54:51 -0400

    Update for newer appveyor installed python versions

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -229,9 +229,7 @@ function InstallPackage ($python_home, $pkg) {
 function main () {
     InstallPython $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     InstallPip $env:PYTHON
-    InstallPackage $env:PYTHON wheel
-    $python_path = $env:PYTHON + "\\python.exe"
-    & $python_path .\\appveyor\\install_cached_package.py numpy
+    InstallPackage $env:PYTHON wheel numpy 
 }
 
 main
