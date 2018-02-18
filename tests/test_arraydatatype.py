2791ff2c0a566df55837156c5b4faf2c467a2861
Mike C. Fletcher <mcfletch@vrplumber.com>
2018-02-18 14:37:42 -0500

    Switch to pytest.mark for skipping, only run pyopengl tests in pyopengl

--- tests/test_arraydatatype.py
+++ tests/test_arraydatatype.py
@@ -10,8 +10,8 @@ try:
 except ImportError as err:
     np = None
 HERE = os.path.abspath(os.path.dirname(__file__))
-
-
+import pytest
+from OpenGL import acceleratesupport
 
 class TestCoreDatatype(basetestcase.BaseTest):
     def test_arrayPointer( self ):
@@ -112,145 +112,158 @@ class TestCoreDatatype(basetestcase.BaseTest):
                         assert not OpenGL.ARRAY_SIZE_CHECKING, """Should not have raised ValueError when not doing array size checking"""
                 finally:
                     glEnd()
-    if np:
-        def test_numpyConversion( self ):
-            """Test that we can run a numpy conversion from double to float for glColorArray"""
-            a = np.arange( 0,1.2, .1, 'd' ).reshape( (-1,3 ))
-            glEnableClientState(GL_VERTEX_ARRAY)
-            try:
-                glColorPointerf( a )
-                glColorPointerd( a )
-            finally:
-                glDisableClientState( GL_VERTEX_ARRAY )
-        def test_glbuffersubdata_numeric(self):
-            from OpenGL.arrays import vbo
-            assert vbo.get_implementation()
-            points = np.array( [
-                [0,0,0],
-                [0,1,0],
-                [1,.5,0],
-                [1,0,0],
-                [1.5,.5,0],
-                [1.5,0,0],
-            ], dtype='f')
-            d = vbo.VBO(points)
-            with d:
-                glBufferSubData(
-                    d.target,
-                    12,
-                    12,
-                    np.array([1,1,1],dtype='f'),
-                )
-    if np and not OpenGL.ERROR_ON_COPY:
-        def test_copyNonContiguous( self ):
-            """Test that a non-contiguous (transposed) array gets applied as a copy"""
+    @pytest.mark.skipif( not np, reason="Numpy not available")
+    def test_numpyConversion( self ):
+        """Test that we can run a numpy conversion from double to float for glColorArray"""
+        a = np.arange( 0,1.2, .1, 'd' ).reshape( (-1,3 ))
+        glEnableClientState(GL_VERTEX_ARRAY)
+        try:
+            glColorPointerf( a )
+            glColorPointerd( a )
+        finally:
+            glDisableClientState( GL_VERTEX_ARRAY )
+    @pytest.mark.skipif( not np, reason="Numpy not available")
+    def test_glbuffersubdata_numeric(self):
+        from OpenGL.arrays import vbo
+        assert vbo.get_implementation()
+        points = np.array( [
+            [0,0,0],
+            [0,1,0],
+            [1,.5,0],
+            [1,0,0],
+            [1.5,.5,0],
+            [1.5,0,0],
+        ], dtype='f')
+        d = vbo.VBO(points)
+        with d:
+            glBufferSubData(
+                d.target,
+                12,
+                12,
+                np.array([1,1,1],dtype='f'),
+            )
+    @pytest.mark.skipif( not np, reason="Numpy not available")
+    @pytest.mark.skipif( OpenGL.ERROR_ON_COPY, reason="Test requires array copy")
+    def test_copyNonContiguous( self ):
+        """Test that a non-contiguous (transposed) array gets applied as a copy"""
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix( )
+        try:
+            transf = np.identity(4, dtype=np.float32)
+            # some arbitrary transformation...
+            transf[0,3] = 2.5
+            transf[2,3] = -80
+            
+            # what do we get with the un-transposed version...
             glMatrixMode(GL_MODELVIEW)
-            glPushMatrix( )
-            try:
-                transf = np.identity(4, dtype=np.float32)
-                # some arbitrary transformation...
-                transf[0,3] = 2.5
-                transf[2,3] = -80
-                
-                # what do we get with the un-transposed version...
-                glMatrixMode(GL_MODELVIEW)
-                glLoadIdentity()
-                glMultMatrixf(transf)
-                untransposed = glGetFloatv(GL_MODELVIEW_MATRIX)
-                # now transposed...
+            glLoadIdentity()
+            glMultMatrixf(transf)
+            untransposed = glGetFloatv(GL_MODELVIEW_MATRIX)
+            # now transposed...
 
-                # with a copy it works...
-                t2 = transf.transpose().copy()
-                # This doesn't work:
-                glLoadIdentity()
-                glMultMatrixf(t2)
-                # This does work:
-                #glMultMatrixf(transf.transpose().copy())
-                transposed = glGetFloatv(GL_MODELVIEW_MATRIX)
+            # with a copy it works...
+            t2 = transf.transpose().copy()
+            # This doesn't work:
+            glLoadIdentity()
+            glMultMatrixf(t2)
+            # This does work:
+            #glMultMatrixf(transf.transpose().copy())
+            transposed = glGetFloatv(GL_MODELVIEW_MATRIX)
 
-                assert not np.allclose( transposed, untransposed ), (transposed, untransposed)
-                
-                t2 = transf.transpose()
-                # This doesn't work:
-                glLoadIdentity()
-                glMultMatrixf(t2)
-                # This does work:
-                #glMultMatrixf(transf.transpose().copy())
-                transposed = glGetFloatv(GL_MODELVIEW_MATRIX)
-                
-                assert not np.allclose( transposed, untransposed ), (transposed, untransposed)
-            finally:
-                glMatrixMode(GL_MODELVIEW)
-                glPopMatrix()
+            assert not np.allclose( transposed, untransposed ), (transposed, untransposed)
+            
+            t2 = transf.transpose()
+            # This doesn't work:
+            glLoadIdentity()
+            glMultMatrixf(t2)
+            # This does work:
+            #glMultMatrixf(transf.transpose().copy())
+            transposed = glGetFloatv(GL_MODELVIEW_MATRIX)
+            
+            assert not np.allclose( transposed, untransposed ), (transposed, untransposed)
+        finally:
+            glMatrixMode(GL_MODELVIEW)
+            glPopMatrix()
 
     def test_bytes_array_support( self ):
         color = b'\000'*12
         glColor3fv( color )
-    if (OpenGL.USE_ACCELERATE) or (sys.version_info[:2] < (3,0)):
-        # ctypes based buffer api does not work on Python 3.x
-        def test_bytearray_support( self ):
-            import struct 
-            data = struct.pack( b'fff', .5, .4, .3 )
-            color = bytearray( data )
-            glColor3fv( color )
-        if sys.version_info[:2] > (2,6):
-            # no memory view object...
-            def test_memoryview_support( self ):
-                color = bytearray( b'\000'*12 )
-                mem = memoryview( color )
-                glColor3fv( mem )
-        def test_buffer_api_basic(self):
-            import array as silly_array
-            structures = [
-                (b'this and that',13,1,True,1,b'B',[13],[1]),
-            ]
-            if sys.version_info[:2] >= (2,7):
-                structures.append(
-                    # on Python 3.4 we do *not* get the (3) prefix :(
-                    ((GLint * 3)( 1,2,3 ),12,4,False,1,[b'(3)<i',b'(3)<l',b'<i'],[3],None),
-                )
-            
-            if sys.version_info[:2] >= (3,0):
-                # only supports buffer protocol in 3.x
-                structures.extend([
-                    (silly_array.array('I',[1,2,3]),12,4,False,1,b'I',[3],[4]),
+        
+        
+    @pytest.mark.skipif( not (
+        OpenGL.USE_ACCELERATE and acceleratesupport.ACCELERATE_AVAILABLE
+    ), reason="Need OpenGL_accelerate for buffer support")
+    def test_bytearray_support( self ):
+        import struct 
+        data = struct.pack( b'fff', .5, .4, .3 )
+        color = bytearray( data )
+        glColor3fv( color )
+    
+    @pytest.mark.skipif( not (
+        OpenGL.USE_ACCELERATE and acceleratesupport.ACCELERATE_AVAILABLE
+    ), reason="Need OpenGL_accelerate for buffer support")
+    def test_buffer_api_basic(self):
+        import array as silly_array
+        structures = [
+            (b'this and that',13,1,True,1,b'B',[13],[1]),
+        ]
+        if sys.version_info[:2] >= (2,7):
+            structures.append(
+                # on Python 3.4 we do *not* get the (3) prefix :(
+                ((GLint * 3)( 1,2,3 ),12,4,False,1,[b'(3)<i',b'(3)<l',b'<i'],[3],None),
+            )
+        
+        if sys.version_info[:2] >= (3,0):
+            # only supports buffer protocol in 3.x
+            structures.extend([
+                (silly_array.array('I',[1,2,3]),12,4,False,1,b'I',[3],[4]),
+            ])
+        try:
+            structures.append( (memoryview(b'this'),4,1,True,1,b'B',[4],[1]) )
+        except NameError:
+            # Python 2.6 doesn't have memory view 
+            pass
+        try:
+            if array:
+                structures.extend( [
+                    (arange(0,9,dtype='I').reshape((3,3)),36,4,False,2,b'I',[3,3],[12,4]),
+                    (arange(0,9,dtype='I').reshape((3,3))[:,1],12,4,False,1,b'I',[3],[12]),
                 ])
-            try:
-                structures.append( (memoryview(b'this'),4,1,True,1,b'B',[4],[1]) )
-            except NameError:
-                # Python 2.6 doesn't have memory view 
-                pass
-            try:
-                if array:
-                    structures.extend( [
-                        (arange(0,9,dtype='I').reshape((3,3)),36,4,False,2,b'I',[3,3],[12,4]),
-                        (arange(0,9,dtype='I').reshape((3,3))[:,1],12,4,False,1,b'I',[3],[12]),
-                    ])
-            except NameError:
-                # Don't have numpy installed...
-                pass
-            
-            from OpenGL.arrays import _buffers
-            for object,length,itemsize,readonly,ndim,format,shape,strides in structures:
-                buf = _buffers.Py_buffer.from_object( object, _buffers.PyBUF_STRIDES|_buffers.PyBUF_FORMAT )
-                with buf:
-                    assert buf.len == length, (object,length,buf.len)
-                    assert buf.itemsize == itemsize, (object,itemsize,buf.itemsize)
-                    assert buf.readonly == readonly, (object,readonly,buf.readonly)
-                    assert buf.ndim == ndim, (object,ndim,buf.ndim)
-                    if isinstance( format, list):
-                        assert buf.format in format, (object,format,buf.format)
-                    else:
-                        assert buf.format == format, (object,format,buf.format)
-                    assert buf.shape[:buf.ndim] == shape, (object, shape, buf.shape[:buf.ndim])
-                    assert buf.dims == shape, (object, shape, buf.dims )
-                    assert buf.buf 
-                    if strides is None:
-                        assert not buf.strides 
-                    else:
-                        assert buf.strides[:buf.ndim] == strides, (object, strides, buf.strides[:buf.ndim])
-                assert buf.obj == None, buf.obj
-                del buf
+        except NameError:
+            # Don't have numpy installed...
+            pass
+        
+        from OpenGL.arrays import _buffers
+        for object,length,itemsize,readonly,ndim,format,shape,strides in structures:
+            buf = _buffers.Py_buffer.from_object( object, _buffers.PyBUF_STRIDES|_buffers.PyBUF_FORMAT )
+            with buf:
+                assert buf.len == length, (object,length,buf.len)
+                assert buf.itemsize == itemsize, (object,itemsize,buf.itemsize)
+                assert buf.readonly == readonly, (object,readonly,buf.readonly)
+                assert buf.ndim == ndim, (object,ndim,buf.ndim)
+                if isinstance( format, list):
+                    assert buf.format in format, (object,format,buf.format)
+                else:
+                    assert buf.format == format, (object,format,buf.format)
+                assert buf.shape[:buf.ndim] == shape, (object, shape, buf.shape[:buf.ndim])
+                assert buf.dims == shape, (object, shape, buf.dims )
+                assert buf.buf 
+                if strides is None:
+                    assert not buf.strides 
+                else:
+                    assert buf.strides[:buf.ndim] == strides, (object, strides, buf.strides[:buf.ndim])
+            assert buf.obj == None, buf.obj
+            del buf
+    
+    
+    @pytest.mark.skipif( not (
+        OpenGL.USE_ACCELERATE and acceleratesupport.ACCELERATE_AVAILABLE
+    ), reason="Need OpenGL_accelerate for buffer support")
+    def test_memoryview_support( self ):
+        color = bytearray( b'\000'*12 )
+        mem = memoryview( color )
+        glColor3fv( mem )
+                
 
     def test_void_dp_for_void_dp_is_self( self ):
         array = ctypes.c_voidp( 12 )
@@ -268,11 +281,11 @@ class TestCoreDatatype(basetestcase.BaseTest):
             # expected error, as we don't have a shader there...
             pass
 
-    if np:
-        def test_array_subclass( self ):
-            s = Subclassed([0,1,2,3,4])
-            result = arraydatatype.ArrayDatatype.asArray( s )
-            assert isinstance( result, Subclassed )
+    @pytest.mark.skipif( not np, reason="Numpy not available")
+    def test_array_subclass( self ):
+        s = Subclassed([0,1,2,3,4])
+        result = arraydatatype.ArrayDatatype.asArray( s )
+        assert isinstance( result, Subclassed )
         
 if np:
     class Subclassed( np.ndarray ):
