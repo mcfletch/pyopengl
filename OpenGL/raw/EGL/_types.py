47493f26d4b26e3a66e4070651dbf60e1ccf37f1
Mike C. Fletcher <mcfletch@vrplumber.com>
2018-02-19 13:26:40 -0500

    Github #6 extension types for EGL to byte-strings

--- OpenGL/raw/EGL/_types.py
+++ OpenGL/raw/EGL/_types.py
@@ -3,11 +3,12 @@ import ctypes
 from OpenGL._opaque import opaque_pointer_cls as _opaque_pointer_cls
 from OpenGL import platform as _p
 from OpenGL import extensions 
+from OpenGL._bytes import as_8_bit
 
 class _EGLQuerier( extensions.ExtensionQuerier ):
-    prefix = 'EGL_'
+    prefix = as_8_bit('EGL_')
     assumed_version = [1,0]
-    version_prefix = 'EGL_VERSION_EGL_'
+    version_prefix = as_8_bit('EGL_VERSION_EGL_')
     def getDisplay( self ):
         """Retrieve the currently-bound, or the default, display"""
         from OpenGL.EGL import (
