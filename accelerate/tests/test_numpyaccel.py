3f40571a159e6af3e3a7bd9591e6da827d3df5b6
Mike C. Fletcher <mcfletch@vrplumber.com>
2018-08-10 20:54:55 -0400

    Split out numpy-only tests from common buffer tests

--- accelerate/tests/test_numpyaccel.py
+++ accelerate/tests/test_numpyaccel.py
@@ -16,55 +16,24 @@ from OpenGL._configflags import ERROR_ON_COPY
 import pytest
 pytestmark = pytest.mark.skipif(not numpy, reason="No numpy installed in order to run tests")
 
-class _AccelArray( unittest.TestCase ):
+class _AccelArray( object ):
     handler_class = None
     def setUp( self ):
         self.array = numpy.array( [[1,2,3],[4,5,6]],'f')
         self.handler = self.handler_class()
-        self.eoc_handler = self.handler_class( True )
     def test_from_param( self ):
-        p = self.handler.from_param( self.array )
+        p = self.handler.from_param( self.handler.asArray(self.array ))
         assert isinstance( p, ctypes.c_void_p )
     def test_dataPointer( self ):
         p = self.handler.dataPointer( self.array )
         assert isinstance( p, integer_types)
         assert p == self.array.ctypes.data
-    def test_zeros( self ):
-        p = self.handler.zeros( (2,3,4), 'f' )
-        assert p.shape == (2,3,4)
-        assert p.dtype == numpy.float32
     def test_arraySize( self ):
         p = self.handler.arraySize( self.array )
         assert p == 6, p
     def test_arrayByteCount( self ):
         p = self.handler.arrayByteCount( self.array )
         assert p == 24, p
-    def test_asArray( self ):
-        p = self.handler.asArray( self.array )
-        assert p is self.array 
-    if ERROR_ON_COPY:
-        def test_asArrayConvert( self ):
-            self.failUnlessRaises(
-                error.CopyError,
-                self.handler.asArray,
-                    self.array, GL.GL_DOUBLE 
-            )
-    else:
-        def test_asArrayConvert( self ):
-            p = self.handler.asArray( self.array, GL.GL_DOUBLE )
-            assert p is not self.array 
-            assert p.dtype == numpy.float64
-            p = self.handler.asArray( self.array, 'd' )
-            assert p is not self.array 
-            assert p.dtype == numpy.float64
-    def test_asArrayCopy( self ):
-        a2 = self.array[:,0]
-        assert not a2.flags.contiguous 
-        self.failUnlessRaises(
-            error.CopyError,
-            self.eoc_handler.asArray,
-                a2
-        )
     def test_unitSize( self ):
         p = self.handler.unitSize( self.array )
         assert p == 3, p
@@ -76,19 +45,52 @@ class _AccelArray( unittest.TestCase ):
         p = self.handler.arrayToGLType( self.array )
         assert p == GL.GL_FLOAT
         
+    
+
+@pytest.mark.skipif(not npf,reason="No numpy native format handler available")
+class TestNumpyNative(_AccelArray,unittest.TestCase):
+    handler_class = getattr(npf,'NumpyHandler',None)
+    def setUp(self):
+        super(TestNumpyNative,self).setUp()
+        self.eoc_handler = self.handler_class( True )
+        
+    def test_asArray( self ):
+        p = self.handler.asArray( self.array )
+        assert p is self.array 
+    def test_downconvert( self ):
+        p = self.handler.asArray( numpy.array( [1,2,3],'d'), GL.GL_FLOAT )
+        assert p.dtype == numpy.float32
     def test_zeros_constant( self ):
         z = self.handler.zeros( (2,3,4), GL.GL_FLOAT)
         assert z.shape == (2,3,4)
         assert z.dtype == numpy.float32
-    
-    def test_downconvert( self ):
-        p = self.handler.asArray( numpy.array( [1,2,3],'d'), GL.GL_FLOAT )
+    def test_zeros( self ):
+        p = self.handler.zeros( (2,3,4), 'f' )
+        assert p.shape == (2,3,4)
         assert p.dtype == numpy.float32
+    def test_asArrayCopy( self ):
+        a2 = self.array[:,0]
+        assert not a2.flags.contiguous 
+        self.failUnlessRaises(
+            error.CopyError,
+            self.eoc_handler.asArray,
+                a2
+        )
+    def test_asArrayConvert( self ):
+        self.failUnlessRaises(
+            error.CopyError,
+            self.eoc_handler.asArray,
+                self.array, GL.GL_DOUBLE 
+        )
+    def test_asArrayConvert( self ):
+        p = self.handler.asArray( self.array, GL.GL_DOUBLE )
+        assert p is not self.array 
+        assert p.dtype == numpy.float64
+        p = self.handler.asArray( self.array, 'd' )
+        assert p is not self.array 
+        assert p.dtype == numpy.float64
+
 
 @pytest.mark.skipif(not npf,reason="No numpy native format handler available")
-class TestNumpyNative(_AccelArray):
-    handler_class = npf.NumpyHandler
-
-@pytest.mark.skipif(not npf,reason="No numpy native format handler available")
-class TestBufferAPI(_AccelArray):
-    handler_class = bpf.MemoryviewHandler
+class TestBufferAPI(_AccelArray,unittest.TestCase):
+    handler_class = getattr(bpf,'MemoryviewHandler',None)
