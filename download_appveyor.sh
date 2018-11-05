4fccc93b14a10e3299c992af130eee5986f79e61
Mike C. Fletcher <mcfletch@vrplumber.com>
2018-11-05 00:10:25 -0500

    Add python3.7 to the tox commands

--- download_appveyor.sh
+++ download_appveyor.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-COMMIT=$(git show-ref HEAD | cut -f 1 -d" ")
-appveyor-artifacts -c $COMMIT -o MikeCFletcher -n pyopengl -v download
+COMMIT=$(git show-ref --head HEAD | cut -f 1 -d" ")
+appveyor-artifacts \
+	-c $COMMIT \
+	-o MikeCFletcher \
+	-n pyopengl \
+	--dir ./dist \
+	--no-job-dirs skip \
+	-v \
+	download
