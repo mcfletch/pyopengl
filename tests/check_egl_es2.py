62038dfd905d982d6e37cdfa1b8e6524ee8788fb
Mike C. Fletcher <mcfletch@vrplumber.com>
2019-02-03 13:45:34 -0500

    Fix the egl2 test cases now that I have an egl2 implementation

--- tests/check_egl_es2.py
+++ tests/check_egl_es2.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 from __future__ import print_function
 import egltest
+from numpy import array
+from OpenGL.arrays.vbo import VBO
+from OpenGL.arrays import ArrayDatatype
 from OpenGL.GLES2 import *
 from OpenGL.GLES2 import shaders
 
@@ -9,7 +12,7 @@ def test_gl( ):
     glClearColor( 1,0,0, 0 )
     glClear( GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT )
     
-    print('pre-compile error', glGetError())
+    print('Error before compilation?', glGetError())
     shader = shaders.compileProgram(
         shaders.compileShader( '''#version 130
     attribute vec3 position;
@@ -18,7 +21,7 @@ def test_gl( ):
     }''', GL_VERTEX_SHADER),
         shaders.compileShader( '''#version 130
     void main() {
-        gl_FragColor = vec4( 1,0,0,0 );
+        gl_FragColor = vec4( 0,1,0,.5 );
     }''', GL_FRAGMENT_SHADER),
     )
     vbo = VBO( array([
@@ -27,7 +30,7 @@ def test_gl( ):
         (-1,-1,0),
     ],dtype='f'))
     position_location = glGetAttribLocation(
-        self.shader, 'position'
+        shader, 'position'
     )
     stride = 3*4
     with vbo:
@@ -36,9 +39,10 @@ def test_gl( ):
             stride = 3*4
             glVertexAttribPointer(
                 position_location,
-                3, GL_FLOAT,False, stride, self.vbo
+                3, GL_FLOAT,False, stride, vbo
             )
             glDrawArrays( GL_TRIANGLES, 0, 3 )
+
     
 if __name__ == "__main__":
     test_gl()
