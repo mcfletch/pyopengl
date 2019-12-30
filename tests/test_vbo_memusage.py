637caf43d9614571a0d050b3b6ac5dd0e364a25d
Mike C. Fletcher <mcfletch@vrplumber.com>
2019-12-30 18:48:06 -0500

    Remove flaky test in favour of more robust one below it
    The first memory test had lots of little heuristics and was
    very loose, yet managed to produce false-positives all the time,
    the second does a far more rigorous check with a tiny allowance
    for error and seems to be more reliable.

--- tests/test_vbo_memusage.py
+++ tests/test_vbo_memusage.py
@@ -1,14 +1,19 @@
 import pygamegltest
 import os
 # We have to import at least *one* VBO implementation...
-from OpenGL import GL
-assert GL
+from OpenGL import GL, arrays
 from OpenGL.arrays import vbo
 try:
     import psutil
 except ImportError:
     psutil = None
+try:
+    unicode 
+except NameError:
+    unicode = str 
+    long = int
 import pytest
+import gc
 try:
     import numpy as np
 except ImportError:
@@ -20,29 +25,9 @@ def get_current_memory():
 @pytest.mark.skipif(not psutil,reason='No psutil available')
 @pytest.mark.skipif(not np,reason='No Numpy available')
 @pygamegltest.pygametest()
-def test_memory_usage():
-    """Test allocation as reported in Github #5"""
-    start = get_current_memory()
-    np_array = np.zeros( (120000, 1,6),dtype='f')
-    after_np = get_current_memory()
-    buffer = vbo.VBO( np_array )
-    with buffer:
-        after_transfer = get_current_memory()
-    
-    # 2.8MB buffer, on Python 2.7 is around 2.9MB usage, but on 
-    # Python 3.6 we see a consistently higher memory usage (3.1MB),
-    # somewhat disturbing...
-    assert (after_np - start) <= 3200000, (after_np-start)
-    assert (after_transfer - after_np) <= 3200000, (after_transfer-after_np)
-
-@pytest.mark.skipif(not psutil,reason='No psutil available')
-@pytest.mark.skipif(not np, reason="No numpy available")
-@pygamegltest.pygametest()
 def test_sf_2980896():
     """Test SF#2980896 report of memory leak on VBO transfer"""
-    from OpenGL.arrays import vbo
-
-    data = np.arange(1000).astype(np.float32)
+    data = arrays.GLfloatArray.zeros((1000,))
     memory = get_current_memory()
     for i in range(100): 
         new_vbo = vbo.VBO(data)
@@ -51,6 +36,8 @@ def test_sf_2980896():
             assert new_vbo is not None, new_vbo
         new_vbo.delete()
         del new_vbo 
+        gc.collect()
+        GL.glFinish()
         if i  < 1: 
             # the *first* call can load lots of libraries, etc...
             memory = get_current_memory()
