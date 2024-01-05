fbe0fab7947788039cb4fbc9a5a1ea65a0c0e15b
Florian Weimer <fweimer@redhat.com>
2024-01-05 08:48:12 +0100

    accelerate: Fix type of PyArray_FillWithScalar
    The first argument is of type PyArrayObject, not PyObject.

--- accelerate/src/numpy_formathandler.pyx
+++ accelerate/src/numpy_formathandler.pyx
@@ -21,7 +21,7 @@ cdef extern from "numpy/arrayobject.h":
     int PyArray_ISCARRAY_RO( np.ndarray instance )
     cdef np.ndarray PyArray_Zeros(int nd, np.Py_intptr_t* dims, np.dtype, int fortran)
     cdef np.ndarray PyArray_EnsureArray(object)
-    cdef int PyArray_FillWithScalar(object, object)
+    cdef int PyArray_FillWithScalar(np.ndarray, object)
     cdef void import_array()
     cdef void* PyArray_DATA( np.ndarray )
     cdef int PyArray_NDIM( np.ndarray )
